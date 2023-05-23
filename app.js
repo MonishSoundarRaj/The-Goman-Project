4a56b1d5b3352b4c9d744830bcf17cf4901c1cf3
Monish Soundar Raj <msoundar@uncc.edu>
2023-05-23 17:58:12 -0400

    add files and images

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.get("/home", (req,res) => {
-    const filePath = path.join(__dirname, 'src', 'home.html')
-    res.sendFile(filePath)
-})
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.listen(process.env.PORT||3000, () => {
-    console.log("Server has started and running")
-})
+app.get("", (req, res) => {
+  res.render("home");
+});
+app.get("/history", (req, res) => {
+  res.render("history");
+});
+app.get("/images", (req, res) => {
+  res.render("images");
+});
+app.get("/documents", (req, res) => {
+  res.render("documents");
+});
+app.get("/home", (req, res) => {
+  res.render("videos");
+});
+app.get("/bibliography", (req, res) => {
+  res.render("bibliography");
+});
+app.get("/ext-links", (req, res) => {
+  res.render("ext-links");
+});
+app.get("/recorded-videos", (req, res) => {
+  res.render("recorded-videos");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server has started and running");
+});
